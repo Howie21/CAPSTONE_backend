e9a5fd55f32fd0b810fee3b969877380cb0c52a7
Howie21 <bryce9643@gmail.com>
2021-12-20 11:55:05 -0500

    Added CRUD Functionality to PropertiesTable via Controller

--- eCommerceStarterCode/Controllers/PropertiesController.cs
+++ eCommerceStarterCode/Controllers/PropertiesController.cs
@@ -1,43 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace eCommerceStarterCode.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/properties")]
     [ApiController]
     public class PropertiesController : ControllerBase
     {
+
+        private readonly ApplicationDbContext _context;
+        public PropertiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/<PropertiesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var properties = _context.Properties;
+            return Ok(properties);
         }
 
         // GET api/<PropertiesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var property = _context.Properties.Where(p => p.Id == id).SingleOrDefault();
+            return Ok(property);
         }
 
         // POST api/<PropertiesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Property value)
         {
+            _context.Properties.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
         }
 
         // PUT api/<PropertiesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public IActionResult Put([FromBody] Property value)
         {
+            _context.Properties.Update(value);
+            _context.SaveChanges();
+            return Ok(value);
         }
 
         // DELETE api/<PropertiesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var property = _context.Properties.Where(p => p.Id == id).SingleOrDefault();
+            _context.Properties.Remove(property);
+            _context.SaveChanges();
+            return StatusCode(200, property);
         }
     }
 }
