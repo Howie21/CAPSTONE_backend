c5a361b98a99ce6ec5781843fce0c0188cad5f3f
Howie21 <bryce9643@gmail.com>
2021-12-27 16:33:07 -0500

    Moving to another system

---
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using eCommerceStarterCode.Configuration;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceStarterCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<TenantInfo> TenantsInfo { get; set; }
+        public DbSet<Lease> Leases { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RolesConfiguration());
+        }
+
+    }
+}
