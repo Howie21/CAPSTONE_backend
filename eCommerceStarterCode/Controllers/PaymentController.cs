dea55a714628be8398bbd835360777b61805105c
Howie21 <bryce9643@gmail.com>
2021-12-29 13:47:43 -0500

    Added Payment table to DB

---
+++ eCommerceStarterCode/Controllers/PaymentController.cs
@@ -0,0 +1,68 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public PaymentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: api/<PaymentController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var payments = _context.Payments.Include(pm => pm.User).Include(pm => pm.Property);
+            return Ok(payments);
+        }
+
+        // GET api/<PaymentController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var payment = _context.Payments.Where(pm => pm.UserId == id ).Include(pm => pm.User).Include(pm => pm.Property);
+            return Ok(payment);
+        }
+
+        // POST api/<PaymentController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Payment value)
+        {
+            _context.Payments.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        // PUT api/<PaymentController>/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Payment value)
+        {
+            _context.Payments.Update(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        // DELETE api/<PaymentController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var payment = _context.Payments.Where(pm => pm.Id == id).SingleOrDefault();
+            _context.Payments.Remove(payment);
+            _context.SaveChanges();
+            return StatusCode(200, payment);
+        }
+    }
+}
---
+++ eCommerceStarterCode/Models/Payment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Payment
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Property")]
+        public int PropertyId { get; set; }
+        public Property Property { get; set; }
+
+        public string DateAndTime { get; set; }
+        public string PaymentAmount { get; set; }
+        
+
+    }
+}
